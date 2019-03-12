daf834bb67eaca64fa503d314e3169a5c764fa1e
mark <markgromano31@gmail.com>
2019-03-11 20:38:07 -0400

    try 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 
 app.get('/', (req, res) => {
   console.log('!')
-  res.sendFile(path.resolve('jsct-fundamentals.pdf'))
+  res.sendFile(path.resolve('assets','jsct-fundamentals.pdf'))
 })
 
 require('dns').lookup(require('os').hostname(), function (err, address, fam) {
