06a73e186cccdadb3ccf9f7110f4386a42da3c0a
Mark Romano <markgromano31@gmail.com>
2019-05-12 23:20:36 -0400

    smol

--- index.js
+++ index.js
@@ -2,12 +2,7 @@ const app = require('express')()
 const path = require('path')
 
 app.get('/', (req, res) => {
-  console.log('!')
-  res.sendFile(path.resolve('assets','jsct-fundamentals.pdf'))
-})
-
-require('dns').lookup(require('os').hostname(), function (err, address, fam) {
-  console.log('IP:', address)
+  res.sendFile(path.resolve('assets','smol.pdf'))
 })
 
 app.listen(3000, () => console.log('listening on 3000'))
